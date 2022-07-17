0f9bd6fe3b37a5591afe27cf6b5869d695d831dd
otherRick <drckbr@gmail.com>
2022-07-17 00:44:01 -0300

    feat: create login validation

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, { Request } from 'express';
 import fs from 'fs';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import tempDatabase from './temp-database.json';
 
 const app = express();
@@ -11,6 +12,52 @@ export interface CreateResponseBody {
   email: string;
   password: string;
 }
+
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400).send({
+      message: 'Missing required fields',
+      status: 'error',
+    });
+
+    return;
+  }
+
+  const foundUser = tempDatabase.find((user) => email === user.email);
+
+  if (!foundUser) {
+    res.status(404).send({
+      message: 'User not found',
+      status: 'error',
+    });
+
+    return;
+  }
+
+  const validPassword = await bcrypt.compare(password, foundUser.password);
+
+  if (!validPassword) {
+    res.status(404).send({
+      message: 'User not found',
+      status: 'error',
+    });
+
+    return;
+  }
+
+  // todo: generate a token that expires!
+  const token = jwt.sign({ email }, 'qualquer');
+
+  res.status(201).send({
+    message: '',
+    status: 'success',
+    data: {
+      token: `Bearer ${token}`,
+    },
+  });
+});
 
 app.post('/create', (req: Request<any, any, CreateResponseBody>, res) => {
   // todo: create validations
