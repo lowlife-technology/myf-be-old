3ce6601f10eb05ee0a0982b2529fc12ae9a0c07b
otherRick <drckbr@gmail.com>
2022-07-22 20:08:45 -0300

    Co-authored-by: daniel anthony <danielanthonyl@users.noreply.github.com>

--- index.ts
+++ index.ts
@@ -1,14 +1,16 @@
 import express from 'express';
+// import Create from './src/categories/controller/Create';
 
-import loginRouter from './src/identity/controller/login';
-import registerRouter from './src/identity/controller/register';
-import categoryController from './src/categories/controller';
+// import loginRouter from './src/identity/controller/login';
+// import registerRouter from './src/identity/controller/register';
+// import categoryController from './src/categories/controller';
 
 const app = express();
 app.use(express.json());
-app.use(loginRouter);
-app.use(registerRouter);
-app.use(categoryController);
+// app.use(loginRouter);
+// app.use(registerRouter);
+// app.use(categoryController);
+app.post('/asd', (req, res) => res.status(201));
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
