6f5639e7ab9acf94ec7687bc931f12e6613c3f10
otherRick <drckbr@gmail.com>
2022-07-28 19:41:53 -0300

    Co-authored-by: daniel anthony <danielanthonyl@users.noreply.github.com>

--- app.ts
+++ app.ts
@@ -1,8 +1,19 @@
 import express from 'express';
 
+import AWS from 'aws-sdk';
 import loginRouter from './src/identity/controller/login';
 import registerRouter from './src/identity/controller/register';
 import categoryController from './src/categories/controller';
+import sendingEmail from './src/identity/services/sendingEmail';
+
+// TODO: create a file to load and check if credentials are loaded correctly.
+AWS.config.getCredentials((err, credentials) => {
+  if (err) {
+    console.log({ err });
+  } else {
+    console.log({ credentials });
+  }
+});
 
 const app = express();
 app.use(express.json());
@@ -10,9 +21,11 @@ app.use(loginRouter);
 app.use(registerRouter);
 app.use(categoryController);
 
-app.get('/', (_, res) => {
+app.get('/', async (_, res) => {
+  await sendingEmail();
+
   res.status(200).send({
-    message: 'runing!',
+    message: 'runing!'
   });
 });
 
