5e48f3dfca511a8347567f07570726e59bef68b5
danielanthonyl <danskyjs@icloud.com>
2022-08-26 20:32:17 -0300

    feature: add authorization to category get and list

--- src/Categories/controller/Get.ts
+++ src/Categories/controller/Get.ts
@@ -1,46 +1,61 @@
-import { PrismaClient } from '@prisma/client';
 import { Request, Response } from 'express';
+import { prisma } from '../../../prisma';
 
 interface getCategoryByIdParams {
   id: string;
 }
 
-const prisma = new PrismaClient();
-
 export default async (req: Request<getCategoryByIdParams>, res: Response) => {
   const { id } = req.params;
+  const { authorization } = req.headers;
 
-  console.log('get', req);
+  if (!authorization) {
+    return res.status(401).send({
+      message: 'unauthorized',
+    });
+  }
 
   if (!id) {
-    res.status(400).send({
+    return res.status(400).send({
       message: 'Missing required id',
       status: 'error',
     });
+  }
 
-    return;
+  const user = await prisma.bearer.findFirst({
+    where: {
+      token: authorization,
+    },
+    include: {
+      identity: true,
+    },
+  });
+
+  if (!user.userId) {
+    return res.status(401).send({
+      message: 'unauthorized',
+    });
   }
 
   try {
-    const getSingleCategory = await prisma.category.findUnique({
+    const getSingleCategory = await prisma.category.findFirst({
       where: {
         id,
+        userId: user.userId,
       },
     });
 
     if (!getSingleCategory) {
-      res.status(400).send({
+      return res.status(400).send({
         message: 'Category not found',
         status: 'error',
       });
-
-      return;
     }
 
-    res.status(200).send(getSingleCategory);
+    return res.status(200).send(getSingleCategory);
   } catch (error) {
-    res.status(500).send({
-      message: 'Internal server error',
+    return res.status(500).send({
+      message: error,
       status: 'error',
     });
   }
